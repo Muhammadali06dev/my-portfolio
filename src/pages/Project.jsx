b3e4c4cad90c61c7f17c4c86300207af06c0199e
Muhammadali06dev <muhammadalideveloper2006@gmail.com>
2023-10-01 15:45:25 +0500

    fix bug

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { projects } from "../helpers/ProjectsList";
+import { projects } from "../helper/projectList";
 import gitHubIcon from "../assets/gitHub.svg"
 
 const Project = () => {
